0a7bfd6f879f7750bf8e39e6a78411e372c6b092
Магомед Бузуртанов <qzombieq_buzurtanov@mail.ru>
2023-10-07 14:57:22 +0300

    Код изменен

--- HelloWorld.pas
+++ HelloWorld.pas
@@ -1,3 +1,19 @@
-﻿begin
-  print('Hello World');
+﻿var
+  Language: string;
+begin
+  WriteLn('Выберите Язык: ru, en, fr');
+  ReadLn(Language);
+  if (Language <> 'ru') and (Language <> 'en') and (Language <> 'fr') then
+  begin
+    WriteLn('Ошибка, выберите указанный язык');
+  end
+  else
+  begin
+    if (Language = 'ru') then
+      WriteLn('Привет!')
+    else if (Language = 'en') then
+      WriteLn('Hello!')
+    else if (Language = 'fr') then
+      WriteLn('Bonjour!');
+  end;
 end.
